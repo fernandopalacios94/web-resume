a3cbd60cb9926159f4fb41be6b16031fa2e3eda0
Fernando Palacios <fpalacios0894@hotmail.com>
2018-07-31 22:28:39 -0500

    Added information of important leaders

---
+++ assets/scripts/people.js
@@ -0,0 +1,23 @@
+var people = [
+{
+	name      : "The Obama Family",
+	ocupation : 'Política'
+	quotes    : ['Don\'t be afraid. Be focused. Be determined. Be hopeful. Be empowered.']
+},
+{
+	name      : 'Benedict Cumberbatch',
+	ocupation : 'Actor',
+	quotes    : [],
+},
+{
+	name      : 'Diana, Princess Of Wales',
+	ocupation : 'Humanitaria y Filántropo',
+	quote     : ['HIV does not make people dangerous to know, so you can shake their hands and give them a hug: Heaven knows they need it']
+},
+{
+	name      : 'Bindi Irwin',
+	ocupation : 'Conservadorista',
+	quote     : ['Every time you lose an animal, it\'s like losing a brick from the house. Pretty soon the house just falls down, you know?']
+}
+
+];
